fb47b2fbd392816d15111a3fae9e66b408ce69ae
gyanendramishra0606 <gyanendra.mishra@gmail.com>
2019-10-07 15:48:02 +0530

    Initial addition of a project
    This is the first version which finds solutions to Pythagoras equation between two given numbers. This provides duplicate solutions as well...which needs to be removed.

---
+++ PythagorasSolutions/PythagorasSolutions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PythagorasSolutions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int from = 1;
+            int to = 50;
+            FindPythagorasSolutions(from, to);
+        }
+
+        private static void FindPythagorasSolutions(int from, int to)
+        {
+            for(int i = from; i <= to; ++i)
+            {
+                for (int j = from; j <= to; ++j)
+                {
+                    //check if c^2 = a^2 + b^2
+                    int cSquare = i * i + j * j;
+
+                    for (int k = from; k <= to; ++k)
+                    {
+                        if (cSquare == k * k)
+                        {
+                            Console.WriteLine("Solution is {0}, {1}, {2}", i, j, k);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
